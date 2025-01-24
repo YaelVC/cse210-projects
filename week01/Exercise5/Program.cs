1ba8bd9d51b4db939c416f0a4fbea7fcba334b15
Dayniz <dyvelascoc@desarrollo-ultrasist.com.mx>
2025-01-23 21:28:40 -0600

    Add Exercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -5,5 +5,44 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+
+        string DisplayWelcome()
+        {
+            return "Welcome to the Program";
+        }
+
+        string PromptUserName()
+        {
+            Console.Write("What is your name?: ");
+            string name = Console.ReadLine();
+
+            return name;
+        }
+
+        int PromptUserNumber()
+        {
+            Console.Write("What is your favorite number?: ");
+            int number = int.Parse(Console.ReadLine());
+
+            return number;
+        }
+
+        int SquareNumber(int number)
+        {
+            int square = number * number;
+            return square;
+        }
+
+        string DisplayResult(string name, int number)
+        {
+            return $"{name}, the square of your number is {number}";
+        }
+
+
+        Console.WriteLine($"{DisplayWelcome()}");
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int square = SquareNumber(number);
+        Console.WriteLine(DisplayResult(name, square)); 
     }
 }
